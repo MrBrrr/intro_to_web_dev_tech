0ad10a205b52349df0067ce98f35f64a437dd0a4
Dominika Trojanowska <d.trojanowsk@gmail.com>
2023-01-11 15:28:36 +0100

    dynamic active elements in nav bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 function ButtonClicked(){
-    alert("Button was clicked");
+    alert('Button was clicked');
 }
+
+$(function(){
+    var pages = ['index', 'about', 'contact'];
+    var pathname = window.location.pathname;  // get the URL
+
+    $('.nav-link').each(function(i){
+        if(pathname.includes(pages[i])){
+            $(this).addClass('active');
+            $(this).attr('aria-current', 'page');
+        }else if(this.className.includes('active')){
+            $(this).removeClass('active');
+        }
+    });
+})
